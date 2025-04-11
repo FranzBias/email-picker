aac5e45709bb7375aab49161464de3bd489e9dcc
Franz Bias <113999853+FranzBias@users.noreply.github.com>
2025-04-11 14:00:00 +0200

    Update launch_email.sh

--- launch_email.sh
+++ launch_email.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Python GUI that allows you to select one of multiple predefined email addresses and automatically copy it to your clipboard.
+# Copyright (c) 2025 FranzBias
+# https://github.com/FranzBias
+#
+# Licensed under the MIT License. See LICENSE file in the root for full license.
+
 # Activate virtual environment and run the script
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$DIR/venv/bin/activate"
